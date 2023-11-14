5c3ea90d3b59283bb0234559dc6da55e752ed263
Hubert <hubertstawicki@gmail.com>
2023-11-14 19:15:43 +0100

    zmiany w server.js

--- server.js
+++ server.js
@@ -69,6 +69,7 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 // Obsługa żądania diety
+const dietLogicInstance = dietLogic(connection, s3Client);
 app.post('/kalkulator', async (req, res) => {
     try {
         const userData = req.body;
